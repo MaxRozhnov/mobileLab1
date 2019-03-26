0fcc7d70e439ab83fb042b4aa8a94e646ebae02e
Maksim Razhnou <maksim.razhnou@itechart-group.com>
2019-03-26 23:50:48 +0300

    Finished polishing user experience with keyboard

--- MobileLab1/RegistrationController.swift
+++ MobileLab1/RegistrationController.swift
@@ -17,42 +17,116 @@ class RegistrationController: UIViewController {
     @IBOutlet private var aboutTextView: UITextView!
     @IBOutlet private var contentView: UIView!
     @IBOutlet private var scrollView: UIScrollView!
+    @IBOutlet private var constraintContentHeight: NSLayoutConstraint!
+
+    @IBOutlet private var loginTextField: UITextField!
+    @IBOutlet private var passwordTextField: UITextField!
+    @IBOutlet private var repeatPasswordTextField: UITextField!
+
+    private var lastOffset: CGPoint?
+    private var activeField: UITextField?
+    private var aboutFieldIsActive: Bool = false
+    private var keyboardHeight: CGFloat?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.hideKeyboardWhenTappedAround()
+        self.hideKeyboardWhenTappedAround()
         self.aboutTextView.delegate = self
         self.aboutTextView.textColor = .lightGray
         self.aboutTextView.layer.borderWidth = 0.5
         self.aboutTextView.layer.cornerRadius = 5.0
         self.aboutTextView.layer.borderColor = UIColor.lightGray.cgColor
-//        self.miscellaneousTextView.text = "Please tell us about yourself in a few words."
-//
-//        NotificationCenter.default.addObserver(
-//            self,
-//            selector: #selector(keyboardWillShow),
-//            name: UIResponder.keyboardWillShowNotification,
-//            object: nil
-//        )
-        // Do any additional setup after loading the view.
+
+        self.firstNameTextField.delegate = self
+        self.middleNameTextField.delegate = self
+        self.lastNameTextField.delegate = self
+        self.loginTextField.delegate = self
+        self.passwordTextField.delegate = self
+        self.repeatPasswordTextField.delegate = self
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(validateUserData))
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
 
     @objc func keyboardWillShow(_ notification: Notification) {
+        guard keyboardHeight == nil else {
+            return
+        }
         if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
             let keyboardRectangle = keyboardFrame.cgRectValue
-            let keyboardHeight = keyboardRectangle.height
+            keyboardHeight = keyboardRectangle.height
+            UIView.animate(withDuration: 0.3) {
+                self.constraintContentHeight.constant += self.keyboardHeight ?? 0
+            }
+            let frameOrigin = activeField?.frame.origin.y ?? aboutTextView.frame.origin.y
+            let frameHeight = activeField?.frame.size.height ?? aboutTextView.frame.size.height
+            changeScrollViewOffset(frameOrigin: frameOrigin, frameHeight: frameHeight)
         }
-        if let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double {
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        UIView.animate(withDuration: 0.3) {
+            self.constraintContentHeight.constant -= self.keyboardHeight ?? 0
+            self.scrollView.contentOffset = self.lastOffset ?? CGPoint()
         }
-        if let animationCurve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt {
+        keyboardHeight = nil
+    }
+
+    @objc func validateUserData() {
+        var dataIsValid = true
+        if let personName = firstNameTextField.text {
+            if personName.isEmpty {
+                //highlight field
+                dataIsValid = false
+            }
+        }
+    }
+
+    func changeScrollViewOffset(frameOrigin: CGFloat, frameHeight: CGFloat) {
+        let distanceToBottom = self.scrollView.frame.size.height - frameOrigin - frameHeight
+        var collapseSpace: CGFloat = keyboardHeight ?? 335.0
+        collapseSpace -= distanceToBottom
+        if collapseSpace < 0 {
+            return
+        }
+        UIView.animate(withDuration: 0.3) {
+            if let xValue = self.lastOffset?.x {
+                self.scrollView.contentOffset = CGPoint(x: xValue, y: collapseSpace + 10)
+            }
         }
     }
 }
 
 extension RegistrationController: UITextViewDelegate {
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        aboutFieldIsActive = true
+        let frameOrigin = aboutTextView.frame.origin.y
+        let frameHeight = aboutTextView.frame.size.height
+        changeScrollViewOffset(frameOrigin: frameOrigin, frameHeight: frameHeight)
+        lastOffset = self.scrollView.contentOffset
+        return true
+    }
+
+    func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        aboutFieldIsActive = false
+        return true
+    }
+
     func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.textColor == UIColor.lightGray {
-            print("nilled")
             textView.text = nil
             textView.textColor = UIColor.black
         }
@@ -63,8 +137,25 @@ extension RegistrationController: UITextViewDelegate {
             textView.text =  "Please tell us about yourself in a few words."
             textView.textColor = UIColor.lightGray
         }
-//        UIView.animate(withDuration: animationDuration) {
-//            self.view.frame.origin.y = 0
-//        }
+    }
+}
+
+extension RegistrationController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        activeField = textField
+        lastOffset = self.scrollView.contentOffset
+        return true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        lastOffset = self.scrollView.contentOffset
+        changeScrollViewOffset(frameOrigin: textField.frame.origin.y,
+                               frameHeight: textField.frame.size.height)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        activeField = nil
+        return true
     }
 }
